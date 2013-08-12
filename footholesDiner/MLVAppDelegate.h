2033a3d2d4b6af4cce671bfe7f3624b34cb5be66
Lance Velasco <lance@mlvgroup.com>
2013-08-12 16:47:33 -0700

    Initial Commit

---
+++ footholesDiner/MLVAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MLVAppDelegate.h
+//  footholesDiner
+//
+//  Created by Lance Velasco on 8/12/13.
+//  Copyright (c) 2013 MLV Group. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MLVAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
